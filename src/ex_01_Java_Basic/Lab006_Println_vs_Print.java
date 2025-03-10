4aea620ea41e2424da8c5648f1e7912f5f8c9776
admin <mrunalmmuley6890@gmail.com>
2025-03-10 08:48:21 +0530

    10th March 2025, Commiting all basic exercises of java

---
+++ src/ex_01_Java_Basic/Lab006_Println_vs_Print.java
@@ -0,0 +1,12 @@
+package ex_01_Java_Basic;
+
+import java.util.Arrays;
+
+public class Lab006_Println_vs_Print {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("How are you?");
+        System.out.print("My name is Mrunal");
+        System.out.print("My profession is Quality Analyst");
+    }
+}
