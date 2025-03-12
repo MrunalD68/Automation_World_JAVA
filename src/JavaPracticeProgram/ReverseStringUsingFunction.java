902f655cd612b51a1c3c58c98ff1b20765c52537
admin <mrunalmmuley6890@gmail.com>
2025-03-12 07:13:28 +0530

    12th March 2025, Commit and push Reverse string program

---
+++ src/JavaPracticeProgram/ReverseStringUsingFunction.java
@@ -0,0 +1,30 @@
+package JavaPracticeProgram;
+
+import java.util.Scanner;
+
+public class ReverseStringUsingFunction {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a String: ");
+        String input = sc.nextLine();
+        sc.close();
+        String reversed = reverseString(input);
+        System.out.println("Reversed String is : " + reversed);
+
+    }
+
+    public static String reverseString(String str) {
+        char[] charArray = str.toCharArray();
+        int left=0,right=charArray.length-1;
+        while(left<right)
+        {
+            char temp =charArray[left];
+            charArray[left]=charArray[right];
+            charArray[right]=temp;
+            left++;
+            right--;
+        }
+        return new String(charArray);
+
+    }
+}
