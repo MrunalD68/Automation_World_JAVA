372640b56c8d716b4ca5f2f158405c1ff100aa49
admin <mrunalmmuley6890@gmail.com>
2025-03-27 05:43:04 +0530

    27 March

---
+++ src/JavaPracticeProgram/ConvertLowercase.java
@@ -0,0 +1,12 @@
+//Convert String to Lower Case
+
+
+package JavaPracticeProgram;
+
+public class ConvertLowercase {
+    public static void main(String[] args) {
+        String text ="Good Morning";
+        String lower = text.toLowerCase();
+        System.out.println(lower);
+    }
+}
---
+++ src/JavaPracticeProgram/RemoveSpaces.java
@@ -0,0 +1,15 @@
+//remove leading and trailing spaces
+
+package JavaPracticeProgram;
+
+public class RemoveSpaces {
+    public static void main(String[] args) {
+        String text = "   Hello, Mrunal   " ;
+        String cleanupper= text.trim().toUpperCase();
+        System.out.println(cleanupper);
+    }
+
+}
+
+
+
