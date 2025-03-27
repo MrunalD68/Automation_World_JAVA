55372989ea059c14a78f9004747323ac07c40b97
admin <mrunalmmuley6890@gmail.com>
2025-03-27 05:32:36 +0530

    12th March 2025, Commit and push task

---
+++ src/JavaPracticeProgram/ConvertUppercase.java
@@ -0,0 +1,12 @@
+// Convert to Uppercase
+
+
+package JavaPracticeProgram;
+
+public class ConvertUppercase {
+    public static void main(String[] args) {
+        String text = "Hello Good Morning";
+        String upper = text.toUpperCase();
+        System.out.println(upper);
+    }
+}
