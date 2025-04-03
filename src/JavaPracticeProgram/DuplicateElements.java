8183fb9fb7647223a5960fdafe2bdfcf855c2eca
admin <mrunalmmuley6890@gmail.com>
2025-04-04 05:29:13 +0530

    4th April

---
+++ src/JavaPracticeProgram/DuplicateElements.java
@@ -0,0 +1,23 @@
+package JavaPracticeProgram;
+
+import java.util.HashSet;
+
+public class DuplicateElements {
+    public static void main(String[] args) {
+        int [] arr = {1, 2, 3, 4, 5, 2, 3, 6, 7, 8, 1};
+        duplicate(arr);
+    }
+    public static void duplicate(int[]arr)
+    {
+        HashSet<Integer> seen = new HashSet<>();
+        HashSet<Integer> duplicates = new HashSet<>();
+
+        for (int num :arr){
+            if(!seen.add(num)){
+                duplicates.add(num);
+
+            }
+    }
+        System.out.println("Duplicat Elements : " +duplicates);
+}
+}
