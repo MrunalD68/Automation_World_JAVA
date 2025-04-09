31bb0fdf27bbf2b1ba060d59ffe210f43a9c4800
admin <mrunalmmuley6890@gmail.com>
2025-04-09 05:33:16 +0530

    7th April

---
+++ src/BasicsProgram/Variable.java
@@ -0,0 +1,14 @@
+//Float and Double number Data types
+
+
+package BasicsProgram;
+
+public class Variable {
+    public static void main(String[] args) {
+        float f = 1.23456789f;
+        double d = 1.23456789d;
+
+        System.out.println("Float Number:" +f);
+        System.out.println("Double Number:" +d);
+    }
+}
